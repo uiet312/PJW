898b919e8286177efca031529f4e3cc13f88f2f9
uiet312 <64244851+uiet312@users.noreply.github.com>
2021-01-24 10:49:24 +0900

    Delete Pyhton/Django directory

--- Pyhton/Django/Django-PraticeProject/PracticeProject/signup/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('', views.signupView, name='signup_signupView'),
-    path('email/', views.email, name='signup_email'),
-    path('verifyView/', views.verifyView, name='signup_verifyView'),
-    path('verify/', views.verify, name='signup_verify')
-]
